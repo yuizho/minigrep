9359cb35894011adec0f8a6cc1c718bde710b450
yuizho <yuizho3@gmail.com>
2019-09-22 18:01:42 +0900

    modify main.rs to extract some process of main function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
@@ -15,13 +16,23 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<Error>> {
     let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("somthing went wrong reading the file");
+    // エラーが置きたらエラー値を返す
+    f.read_to_string(&mut contents)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
